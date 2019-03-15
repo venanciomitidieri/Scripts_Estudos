da9dfcce8cda0cc9bf1c79d435b691936efff0be
Venâncio Mitidieri <venanciomitidieri@gmail.com>
2019-03-15 12:29:09 -0300

    Implementação do fizzbuzz

--- fizzbuzztest.py
+++ fizzbuzztest.py
@@ -1,1 +1,15 @@
-fizzbuzztest.py
+def multiplo (numero):
+	if numero % 15 == 0:
+		print ('fizzbuzz')
+
+	elif numero % 3 == 0:
+		print ('fizz')
+
+	elif numero % 5 == 0:
+		print ('buzz')
+
+	else:
+		print (numero)
+
+multiplo (15)
+
