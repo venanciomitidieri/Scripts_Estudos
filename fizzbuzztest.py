3f728de8d6654e777709240fe1edd5f0573c4408
Venâncio Mitidieri <venanciomitidieri@gmail.com>
2019-03-15 15:06:00 -0300

    refatoração do fizzbuzz com Lambda

--- fizzbuzztest.py
+++ fizzbuzztest.py
@@ -1,15 +1,34 @@
-def multiplo (numero):
-	if numero % 15 == 0:
-		print ('fizzbuzz')
+from functools import partial  
 
-	elif numero % 3 == 0:
-		print ('fizz')
+multiple_of = lambda base, num: num % base == 0
+multiple_3 = partial(multiple_of, 3)
+multiple_5 = partial(multiple_of, 5)
 
-	elif numero % 5 == 0:
-		print ('buzz')
 
-	else:
-		print (numero)
+def multiple(number):
 
-multiplo (15)
+    if multiple_3(number) and multiple_5(number):
+        return 'fizzbuzz'
 
+    elif multiple_3(number):
+        return 'fizz'
+
+    elif multiple_5(number):
+        return 'buzz'
+
+    else:
+        return str(number)
+
+
+if __name__ == '__main__':
+    assert 'fizz' == multiple(3)
+    assert 'fizz' == multiple(9)
+    assert 'fizz' == multiple(12)
+
+    assert 'buzz' == multiple(5)
+    assert 'buzz' == multiple(10)
+    assert 'buzz' == multiple(20)
+
+    assert 'fizzbuzz' == multiple(15)
+    assert 'fizzbuzz' == multiple(30)
+    assert 'fizzbuzz' == multiple(45)
